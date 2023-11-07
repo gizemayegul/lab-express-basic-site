f723b62858ae3f82567fbdcac3aec93897c9f246
gizemayegul <gizemayazyegul@gmail.com>
2023-11-07 16:20:53 +0100

    homepage

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+
+//console.log("lol");
+app.use(express.static(__dirname + "/public"));
+//console.log(__dirname, "dirname");
+
+app.get("/home", (request, response, next) => {
+  response.sendFile(__dirname + "/public/views/home.html");
+});
+app.get("/about", (request, response, next) => {
+  response.sendFile(__dirname + "/public/views/about.html");
+});
+app.get("/works", (request, response, next) => {
+  response.sendFile(__dirname + "/public/views/work.html");
+});
+app.get("/gallery", (request, response, next) => {
+  response.sendFile(__dirname + "/public/views/work.html");
+});
+
+app.listen(3000, () => {
+  console.log("the application is running on localhost:3000");
+});
